10401c35b8f3a3aaea45db62eca90f734c96a4bc
ronaltrianat <ronaltriana.t@gmail.com>
2019-04-09 14:35:08 -0500

    update sql model

---
+++ scripts/database/seed_data.sql
@@ -0,0 +1,12 @@
+USE `workflow_process_db` ;
+
+select * from document_types;
+select * from user_status;
+select * from users;
+
+insert into document_types(document_type_id, code, description) values (1, 'CC', 'Cedula de Ciudadania');
+insert into user_status(user_status_id, name, description) values (1, 'Activo', 'Activo');
+insert into user_status(user_status_id, name, description) values (2, 'Inactivo', 'Inactivo');
+
+insert into users(name, surname, age, email, document_number, password, fk_user_status_id, address, fk_document_type_id) 
+values ('Ronal', 'Triana', 30, 'ronaltriana.t@gmail.com', '1093884556', '$2a$10$lxjw1F6NujiIthrktf00ReBaqukdBG/gWAHNwx3Y8eSAHL.sTg3hy', 1, 'Bogota', 'CC');
